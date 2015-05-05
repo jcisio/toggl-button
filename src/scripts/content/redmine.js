bab06f036664d3118989dac056b87f71cfff25aa
Hai-Nam Nguyen <hainam@jcisio.com>
2015-05-05 14:58:06 +0200

    Add a Redmine 'Done' button

--- src/scripts/content/redmine.js
+++ src/scripts/content/redmine.js
@@ -20,4 +20,17 @@ togglbutton.render('body.controller-issues.action-show h2:not(.toggl)', {}, func
   });
 
   $('h2').appendChild(link);
+
+  var done = createTag('button', '', 'Done');
+  $('h2').appendChild(done);
+  done.title = 'Tâche faite, assigne le statut À tester.';
+  done.addEventListener('click', function() {
+    var uid = $('.author .user').href.replace(/^.+?(\d+)$/, '$1');
+    $('.icon.icon-edit').click();
+    $('#issue_status_id option[value="6"]').selected = 'selected';// "À tester."
+    $('#issue_assigned_to_id option[value="' + uid  +'"]').selected = 'selected';
+    $('#issue_done_ratio option[value="100"]').selected = 'selected';
+    $('#issue_custom_field_values_23 option[value="Doing"]').selected = 'selected';
+    $('#issue_notes').focus();
+  });
 });
